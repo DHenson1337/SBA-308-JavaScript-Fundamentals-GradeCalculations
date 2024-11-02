23a2175868bb2acff22c5faab6cf58aee6d5d00d
dhenson <davonhensontech@gmail.com>
2024-11-02 15:43:46 -0400

    Starting over, gonna have to recreate the psuedo code

--- README.md
+++ README.md
@@ -2,35 +2,6 @@
 
 Pseudo Code:
 
-1) Take the learner Submissions group and group them
-so that the Id only appears once.
-
-Turn learner_id into a unique key and
-submissions into an array.
-something like this:
-
-learner_id: 125,
-    submissions: [
-      { assignment_id: 1, submitted_at: "2023-01-25", score: 47 },
-      { assignment_id: 2, submitted_at: "2023-02-12", score: 150 },
-      { assignment_id: 3, submitted_at: "2023-01-25", score: 400 }
-
-Call it studentData: (Assignment Group)
-
-2) Confirm if an AssignmentGroup does belong to the right course.
-create a new variable with that info called:
-confirmedGroup (Course Info)
-
-function confirmAssignmentGroup(courseInfo, assignmentGroup) {
-  return courseInfo.id === assignmentGroup.course_id;
-}
-
-console.log(confirmAssignmentGroup(CourseInfo, AssignmentGroup));
-console.log(CourseInfo.id);
-console.log(AssignmentGroup.course_id);
-
-3) create a new variable for AssignmentGroup.assignments (so I can have the array)
-call the variable homework (LearnerSubmission)
+1) Redo Psuedo code am not understanding the assingment
 
 
-?) Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above
--- courseinfo.js
+++ courseinfo.js
@@ -1,109 +1,126 @@
 //courseinfo.js
 // The provided course information.
 const CourseInfo = {
-    id: 451,
-    name: "Introduction to JavaScript"
-  };
-  
-  // The provided assignment group.
-  const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-      {
-        id: 1,
-        name: "Declare a Variable",
-        due_at: "2023-01-25",
-        points_possible: 50
-      },
-      {
-        id: 2,
-        name: "Write a Function",
-        due_at: "2023-02-27",
-        points_possible: 150
-      },
-      {
-        id: 3,
-        name: "Code the World",
-        due_at: "3156-11-15",
-        points_possible: 500
-      }
-    ]
-  };
-  
-  // The provided learner submission data.
-  const LearnerSubmissions = [
+  id: 451,
+  name: "Introduction to JavaScript"
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
     {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50
     },
     {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
     },
     {
-      learner_id: 125,
-      assignment_id: 3,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 400
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-24",
-        score: 39
-      }
-    },
-    {
-      learner_id: 132,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-03-07",
-        score: 140
-      }
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
     }
-  ];
-  
-  function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    const result = [
-      {
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
-      },
-      {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
-      }
-    ];
-  
-    return result;
+  ]
+};
+
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
   }
-  
-  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
-  console.log(result);
-  
-  //Comment out later
-/*   {learner_id: 125,assignment_id: 1,submission: {submitted_at: "2023-01-25",score: 47}},
-    { learner_id: 125,assignment_id: 2,submission: {submitted_at: "2023-02-12",score: 150}},
-    {learner_id: 125,assignment_id: 3,submission: {submitted_at: "2023-01-25",score: 400}},
-    {learner_id: 132,assignment_id: 1,submission: {submitted_at: "2023-01-24",score: 39} },
-    {learner_id: 132,assignment_id: 2,submission: {submitted_at: "2023-03-07",score: 140 */
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//I don't think this part will work for what the assignment wants me to do.
+/* 
+//Part 1
+// Take the learner Submissions group and group them
+
+function groupLearner(arr) {
+  const group = {};
+
+  //loop through each submission
+  for (let data of arr) {
+    const learner_id = data.learner_id;
+    const assignment_id = data.assignment_id;
+    const submission = data.submission;
+
+
+    if (!group[learner_id]) {
+      group[learner_id] = { learner_id: learner_id, submissions: [] };
+    }
+    //creating an new object with the submissions
+    group[learner_id].submissions.push({
+      assignment_id: assignment_id,
+      submitted_at: submission.submitted_at,
+      score: submission.score
+    })
+  }
+  return Object.values(group);
+}
+console.log(object);
+const studentData = groupLearner(LearnerSubmissions);
+console.log(studentData); */
